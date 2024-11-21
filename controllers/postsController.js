38208a895a95ec1f603d293fc02e04212645a107
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-21 16:25:21 +0100

    add show function

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -18,8 +18,10 @@ function index(req, res) {
 
 //show
 function show (req,res){
-   
-    res.json(post);
+   const id = parseInt(req.params.id);
+   const post = postsData.find(post=> post.id === id);
+
+res.json(post);
 
 }
 // create
--- data/posts.js
+++ data/posts.js
@@ -1,29 +1,34 @@
 const posts =[
     {   
+        id: 1,
         title: "Ciambellone",
         contenuto:"",
         img:"./public/img/ciambellone.jpeg",
         tags : ["ciambellone", "dolce"]
     },
     {   
+        id: 2,
         title: "Cracker alla barbabietola",
         contenuto:"",
         img:"./public/img/cracker_barbabietola.jpeg",
         tags : ["snack","salato"]
     },
     {   
+        id: 3,
         title: "Pane fritto",
         contenuto:"",
         img:"./public/img/cracker_barbabietola.jpeg",
         tags : ["pane", "cina"]
     },
     {   
+        id: 4,
         title: "Pasta alla barbabietola",
         contenuto:"",
         img:"./public/img/pasta_barbabietola.jpeg",
         tags : ["pasta", "barbabietola", "rosso"]
     },
     {   
+        id: 5,
         title: "Torta paesana",
         contenuto:"",
         img:"./public/img/torta_paesana.jpeg",
